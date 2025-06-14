1c8a90dceef699db08a4b1d0f8c76a4ee6bc1b5a
ahsan <ahsan.ullah124259@gmail.com>
2025-06-14 20:17:44 +0600

    first navbar create in daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 import FormPage from './components/FormPage'
 
 function App() {
   
   return (
     <>
-      
+      <DaisyNav></DaisyNav>
       <FormPage></FormPage>
      
     </>
